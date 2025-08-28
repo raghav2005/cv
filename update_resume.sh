5ee8af305e8aa0813b6f698448d5423e8f815750
Raghav <raghavawasthi2005@gmail.com>
2025-08-29 00:33:09 +0100

    updated script to use cpdf to be ats compliant

--- update_resume.sh
+++ update_resume.sh
@@ -2,7 +2,12 @@
 
 # Define the source and target files
 SOURCE=~/github_repos/cv/Resume.pdf
+SOURCE_DIR=~/github_repos/cv
 TARGET=~/OneDrive/Work/Resume.pdf
+
+cpdf -remove-annotations $SOURCE -o $SOURCE_DIR/Resume-ats.pdf;
+rm -rf $SOURCE;
+mv $SOURCE_DIR/Resume-ats.pdf $SOURCE;
 
 # Check if the target symbolic link already exists
 if [ -L "$TARGET" ]; then
